0f2d038b04573a4e7eb860f2ef1a9a7fd9469312
The-Promised-End <tom.tk.li@gmail.com>
2017-11-09 00:30:10 +0000

    TinyApp first commit

---
+++ express_server.js
@@ -0,0 +1,84 @@
+var express = require("express");
+var app = express();
+var PORT = process.env.PORT || 8080; // default port 8080
+const bodyParser = require("body-parser");
+
+function generateRandomString() {
+  return Math.random().toString(36).substr(2,6);
+}
+
+app.set("view engine", "ejs")
+
+
+
+var urlDatabase = {
+  "b2xVn2": "http://www.lighthouselabs.ca",
+  "9sm5xK": "http://www.google.com"
+};
+
+// urlDatabase[id] = stuff here;
+
+// middleware
+app.use(bodyParser.urlencoded({extended: true}));
+
+
+// specific routes
+app.get("/", (req, res) => {
+  res.send("Hello!");
+});
+
+app.get("/urls", (req, res) => {
+  let templateVars = { urls: urlDatabase };
+  res.render("urls_index", templateVars);
+});
+
+app.post("/urls", (req, res) => {
+  console.log(req.body);
+  let shortURL = generateRandomString();
+  let longURL = req.body["longURL"];
+  urlDatabase[shortURL] = longURL;
+  console.log(shortURL);
+  res.redirect("/urls/");
+});
+
+app.get("/urls/new", (req, res) => {
+  let id = req.params.id;
+  res.render("urls_new");
+});
+
+app.get("/urls/:id", (req, res) => {
+  let templateVars = { shortURL: req.params.id,
+                       urlDatabase: urlDatabase };
+  res.render("urls_show", templateVars);
+});
+
+app.post("/urls/:id", (req, res) => {
+  let shortURL = req.params.id;
+  let longURL = req.body["longURL"];
+  urlDatabase[shortURL] = longURL;
+  res.redirect("/urls");
+});
+
+app.get("/u/:shortURL", (req, res) => {
+  let longURL = urlDatabase[req.params.shortURL]
+  console.log(urlDatabase[req.params.shortURL]);
+  res.redirect(longURL);
+});
+
+app.post("/urls/:id/delete", (req, res) => {
+  let id = req.params.id;
+  delete urlDatabase[id];
+  res.redirect("/urls");
+});
+
+
+// app.post("/urls/:id", (req, res) => {
+//   let id = req.params.id
+//   res.redirect(`/urls/{$id}`)
+// }
+
+
+app.listen(PORT, () => {
+  console.log(`Example app listening on port ${PORT}!`);
+});
+
